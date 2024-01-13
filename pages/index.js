8bd267b1a1d6b056ecc945285fa7cbde9b958755
GenuineMarketing <info@genuinemarketing.se>
2024-01-13 15:13:58 +0100

    fast logo fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '@components/Footer.module.css'
+
 
 export default function Home() {
   return (
@@ -15,6 +17,7 @@ export default function Home() {
         <p className="description">
           WORK IN PROGRESS!
         </p>
+        <img src="/genuine-marketing.svg" alt="Genuine marketing logo" className={styles.logo} />
       </main>
 
       <Footer />
